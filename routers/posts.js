f01b0d538110e7d43f793e8acc4719b8ef6cea4f
LindaTancini <lindatancini95@gmail.com>
2025-03-27 15:10:57 +0100

    feat: correct route

--- routers/posts.js
+++ routers/posts.js
@@ -4,27 +4,27 @@ const express = require("express");
 const router = express.Router();
 // SPOSTO LE ROTTE CRUD DA APP IN ROUTER (al posto di app. metto router.)
 // INDEX -> visualizzo tutti gli elementi
-router.get("/posts", (req, res) => {
+router.get("/", (req, res) => {
   res.send("Vedo tutti i post");
 });
 // SHOW -> visualizzo un solo elemento
-router.get("/posts/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   res.send("Vedo un solo post");
 });
 // STORE -> creo un nuovo elemento
-router.post("/posts", (req, res) => {
+router.post("/", (req, res) => {
   res.send("Ho creato un nuovo post");
 });
 // UPDATE -> aggiorno un elemento
-router.put("/posts/:id", (req, res) => {
+router.put("/:id", (req, res) => {
   res.send("Ho aggiornato un post");
 });
 // MODIFY -> modifico parzialmente un elemento
-router.patch("/posts/:id", (req, res) => {
+router.patch("/:id", (req, res) => {
   res.send("Ho modificato parzialmente un post");
 });
 // DESTROY -> cancello un elemento
-router.delete("/posts/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
   res.send("Ho cancellato un post");
 });
 //ESPORTO ROUTER IN APP
