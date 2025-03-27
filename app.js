95f17a74d9a062c92dfe0f3da6b60cb65f5dbb7b
LindaTancini <lindatancini95@gmail.com>
2025-03-27 14:19:31 +0100

    feat: create route index

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express = require("express");
 const app = express();
 // DEFINISCO LA PORTA
 const port = 3000;
-
+// CREO LE ROTTE CRUD
+// INDEX -> visualizzo tutti gli elementi
+app.get("/posts", (req, res) => {
+  res.send("Vedo tutti i post");
+});
 // AVVIO IL SERVER SULLA PORTA 3000 E CONTROLLO SU POSTMAN
 app.listen(port, () => {
   console.log(`Sono un server attivo sulla porta:${port}`);
