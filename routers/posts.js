df8767d982d206e07b04291c98d5fe2d59f5a4c7
LindaTancini <lindatancini95@gmail.com>
2025-03-27 16:11:44 +0100

    feat: make second bonus

--- routers/posts.js
+++ routers/posts.js
@@ -2,34 +2,6 @@
 const express = require("express");
 // CREO IL ROUTER
 const router = express.Router();
-// SPOSTO LE ROTTE CRUD DA APP IN ROUTER (al posto di app. metto router.)
-// INDEX -> visualizzo tutti gli elementi (in formato JSON)
-router.get("/", (req, res) => {
-  res.json(posts); // LISTA JSON IN POSTMAN
-});
-// SHOW -> visualizzo un solo elemento
-router.get("/:id", (req, res) => {
-  res.send("Vedo un solo post");
-});
-// STORE -> creo un nuovo elemento
-router.post("/", (req, res) => {
-  res.send("Ho creato un nuovo post");
-});
-// UPDATE -> aggiorno un elemento
-router.put("/:id", (req, res) => {
-  res.send("Ho aggiornato un post");
-});
-// MODIFY -> modifico parzialmente un elemento
-router.patch("/:id", (req, res) => {
-  res.send("Ho modificato parzialmente un post");
-});
-// DESTROY -> cancello un elemento
-router.delete("/:id", (req, res) => {
-  res.send("Ho cancellato un post");
-});
-//ESPORTO ROUTER IN APP
-module.exports = router;
-
 // ARRAY DEI POSTS
 const posts = [
   {
@@ -82,3 +54,37 @@ const posts = [
     ],
   },
 ];
+
+// SPOSTO LE ROTTE CRUD DA APP IN ROUTER (al posto di app. metto router.)
+// INDEX -> visualizzo tutti gli elementi (in formato JSON)
+router.get("/", (req, res) => {
+  res.json(posts); // LISTA JSON IN POSTMAN
+});
+// SHOW -> visualizzo un solo elemento
+router.get("/:slug", (req, res) => {
+  const currentPostSlug = req.params.slug;
+  const currentPost = posts.find((element) => element.slug === currentPostSlug);
+  if (currentPost) {
+    res.json(currentPost);
+  } else {
+    res.json("errore");
+  }
+});
+// STORE -> creo un nuovo elemento
+router.post("/", (req, res) => {
+  res.send("Ho creato un nuovo post");
+});
+// UPDATE -> aggiorno un elemento
+router.put("/:id", (req, res) => {
+  res.send("Ho aggiornato un post");
+});
+// MODIFY -> modifico parzialmente un elemento
+router.patch("/:id", (req, res) => {
+  res.send("Ho modificato parzialmente un post");
+});
+// DESTROY -> cancello un elemento
+router.delete("/:id", (req, res) => {
+  res.send("Ho cancellato un post");
+});
+//ESPORTO ROUTER IN APP
+module.exports = router;
